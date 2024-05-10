5786107ff6142afdf5ccc6d9be739b17095456f1
Robin W. <robin.white@botts.inc>
2024-05-10 12:19:22 -0500

    this is the changes suggested by Nick.

--- sensors/cyber/sensorhub-driver-sysinfo/src/main/java/com/sample/impl/sensor/sysinfo/SystemsInfoSensor.java
+++ sensors/cyber/sensorhub-driver-sysinfo/src/main/java/com/sample/impl/sensor/sysinfo/SystemsInfoSensor.java
@@ -8,7 +8,8 @@
  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  for the specific language governing rights and limitations under the License.
 
- Copyright (C) 2020-2021 Botts Innovative Research, Inc. All Rights Reserved.
+ Copyright (C) 2024 Botts Innovative Research, Inc. All Rights Reserved.
+
  ******************************* END LICENSE BLOCK ***************************/
 package com.sample.impl.sensor.sysinfo;
 
